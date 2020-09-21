e88db70ea6a143005cd6cfc19a13fc00090b100c
rei_shimada <remate.bskshe@gmail.com>
2020-09-21 15:37:27 +0900

    csrf対策

--- www/html/admin.php
+++ www/html/admin.php
@@ -1,4 +1,8 @@
 <?php
+// iframe禁止のためのヘッダー関数
+header('X-FRAME-OPTIONS: DENY');
+
+
 // 設定ファイル読み込み
 require_once '../conf/const.php';
 // 汎用関数ファイル読み込み
@@ -11,10 +15,15 @@ require_once MODEL_PATH . 'item.php';
 // ログインチェックを行うため、セッションを開始する
 session_start();
 
+// セッションのトークンとPOSTで送られてきたトークンの照合
+if($_SESSION['token'] === $_POST['token']){
+
+}
+
 // ログインしていないユーザーがadmin.phpを直接開こうとした場合、ログインページにとばす。
 if(is_logined() === false){
   redirect_to(LOGIN_URL);
-}
+
 
 // データベースに接続
 $db = get_db_connect();
@@ -27,5 +36,11 @@ if(is_admin($user) === false){
 }
 // データベースから商品一覧を取得
 $items = get_all_items($db);
+
+}
+// ランダムなパスワードを一行で生成する。
+$token=substr(base_convert(hash('sha256', uniqid()), 16, 36), 0, 30);
+$_SESSION['token']=$token;
+
 // 管理画面テンプレートファイルの読み込み
 include_once VIEW_PATH . '/admin_view.php';
