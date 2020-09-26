a0691119a4d0ae4bbe6f8f6c1a12f14b7786a051
rei_shimada <remate.bskshe@gmail.com>
2020-09-27 00:49:30 +0900

    4章2-1

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,20 +1,26 @@
-$sql = "
-    SELECT
-      items.item_id,
-      items.name,
-      items.price,
-      items.stock,
-      items.status,
-      items.image,
-      carts.cart_id,
-      carts.user_id,
-      carts.amount
-    FROM
-      carts
-    JOIN
-      items
-    ON
-      carts.item_id = items.item_id
-    WHERE
-      carts.user_id = ?
-  ";
+-- --------------------------------------------------------
+
+-- CURRENT_TIMESTAMP 新規追加した場合に日時が自動登録
+--
+-- テーブルの構造 `history`
+--
+
+CREATE TABLE `history` (
+  `history_id` int(11)  NOT NULL,
+  `user_id`    int(11)  NOT NULL,
+  `created`    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `details`
+--
+
+CREATE TABLE `details` (
+  `history_id` int(11) NOT NULL,
+  `item_id`    int(11) NOT NULL,
+  `amount`     int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
